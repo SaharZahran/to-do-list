a8fac590e37307c4d5559bf29c8e1d076385785b
Sahar Zahran <saharzahran666@yahoo.com>
2021-07-09 14:48:39 +0300

    final changes

--- app.js
+++ app.js
@@ -47,15 +47,15 @@ function saveUserData(e) {
     localStorage.setItem("users", JSON.stringify(arrOfUser));
     console.log(arrOfUser);
   } else {
-    data.push(newUser);
-    localStorage.setItem("users", JSON.stringify(data));
+    terms.addEventListener("click", (e) => {
+      data.push(newUser);
+      localStorage.setItem("users", JSON.stringify(data));
+      signUpPage.style.display = "none";
+      dashBoard.style.display = "block";
+    });
   }
 }
 
-terms.addEventListener("click", (e) => {
-  signUpPage.style.display = "none";
-  dashBoard.style.display = "block";
-});
 //Log in Page
 
 const logInForm = document.getElementById("logInForm");
@@ -88,11 +88,12 @@ function logIn(e) {
   focus.addEventListener("focus", () => {
     alertMessage.textContent = "";
   });
-
+  
   //Dashboard page:
   const dashBoardForm = document.getElementById("dashBoardForm");
   let listNumber = 1;
   const storedLists = JSON.parse(localStorage.getItem("lists"));
+  
   let lists = [];
   storedLists.forEach((list) => {
     if (list.currentUser === currentUserEmail) {
@@ -104,7 +105,7 @@ function logIn(e) {
     div.innerHTML = `
     <br>
     <input type="radio" id="${listNumber}" name="myList">
-    <label for="${listNumber}">${listName}</label>
+    <label for="${listNumber}" id='listNameText'>${listName}</label>
     <br>
     `;
     dashBoardForm.appendChild(div);
@@ -113,54 +114,171 @@ function logIn(e) {
   //To-do-list Page
   const toDoListPage = document.querySelector(".toDoList");
   const userAllList = dashBoardForm.querySelectorAll("input");
-  const ul = toDoListPage.querySelector("#toDoList");
-  let goal ='';
+  const divsContainer = toDoListPage.querySelector("#toDoList");
+  const toDoListForm = toDoListPage.querySelector("#toDoListForm");
+  let goal = "";
   let mainGoal;
-
+  const arr = [];
+  
+  function Done(currentUser, doneItem){
+    this.currentUser = currentUser;
+    this.doneItem = doneItem;
+  }
+  
   userAllList.forEach((input) => {
     input.addEventListener("click", (e) => {
-
+      // localStorage.removeItem('doneItems');
       dashBoard.style.display = "none";
       toDoListPage.style.display = "block";
-      startPage.style.display='none';
-      mainButtons.style.display='block';
+      startPage.style.display = "none";
+      mainButtons.style.display = "block";
 
-       goal = e.target.nextElementSibling.innerText;
-      storedLists.forEach(list=>{
-        if(list.newListName===goal){
-           mainGoal = list;
-           console.log(mainGoal);
+      goal = e.target.nextElementSibling.innerText;
+      arr.push(goal);
+      document.getElementById("head").innerText = goal;
+
+      const liItems = divsContainer.querySelectorAll("div");
+      liItems.forEach((div) => {
+        divsContainer.removeChild(div);
+      });
+      storedLists.forEach((list) => {
+        if (list.newListName === goal) {
+          mainGoal = list;
+        }
+      });
+
+      //2  Check off an item as "done", and uncheck it as well.
+      const arrOfItems = mainGoal.newListItems;
+      let id = 0;
+      arrOfItems.forEach((item) => {
+        id++;
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <br>
+        <input type="checkbox" id="${id}" name="myList">
+        <label for="${id}" id='listNameText'>${item}</label>
+        <br>
+        `;
+        divsContainer.appendChild(div);
+
+        // to show completed items of the list:
+        const allListItems = toDoListPage.querySelectorAll('label');
+        const allDoneItems = JSON.parse(localStorage.getItem('doneItems'));
+        if(allDoneItems !== null){
+          allListItems.forEach(item => {
+           allDoneItems.forEach(done => {
+             if(item.innerText === done.doneItem){
+               item.classList.add('doneItems');
+             }
+           })
+          })
+        }
+      });
+
+      // checkbox
+      const allInputs = divsContainer.querySelectorAll('input');
+      allInputs.forEach(input => {
+        input.addEventListener('click', (e) => {
+          e.target.nextElementSibling.classList.toggle('doneItems');
+
+          const done = new Done(currentUserEmail, e.target.nextElementSibling.innerText);
+          const arrOfDoneItems = [];
+          const arr = [];
+          const storedDoneItems = JSON.parse(localStorage.getItem("doneItems"));
+          if(storedDoneItems){
+            storedDoneItems.forEach(doone =>{
+              arr.push(doone.doneItem)
+            })
+          }
+          if(storedDoneItems == null){
+            arrOfDoneItems.push(done);
+            localStorage.setItem('doneItems', JSON.stringify(arrOfDoneItems));
+          }else if(!arr.includes(done.doneItem)){
+            storedDoneItems.push(done);
+            localStorage.removeItem("doneItems");
+            localStorage.setItem('doneItems', JSON.stringify(storedDoneItems));
         }
       })
-
-      const arrOfItems= mainGoal.newListItems;
-      console.log(arrOfItems);
-      arrOfItems.forEach(item=>{
-        const li = document.createElement("li");
-        li.innerText = item;
-        li.classList.add("items");
-        ul.appendChild(li);
+    })
+    const storedDoneItems = JSON.parse(localStorage.getItem("doneItems"));
+    allInputs.forEach(deletedInput =>{
+      deletedInput.addEventListener('click', (event) => {
+        if(storedDoneItems){
+          storedDoneItems.forEach(unCompleted => {
+           if(unCompleted.doneItem === event.target.nextElementSibling.innerText){
+              storedDoneItems.splice(storedDoneItems.indexOf(unCompleted), 1);
+              localStorage.removeItem('doneItems');
+              localStorage.setItem('doneItems', JSON.stringify(storedDoneItems));
+           }
+          })
+        }
       })
+    })
+  });
+});
+
+  // add new item:
+  let id = 1;
+  toDoListForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const store = JSON.parse(localStorage.getItem("lists"));
+    localStorage.removeItem("'lists");
+    const itemInput = toDoListForm.querySelector("#newItem").value;
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <br>
+    <input type="checkbox" id="${id}" name="myList">
+    <label for="${id}" id='listNameText'>${itemInput}</label>
+    <br>
+    `;
+    divsContainer.appendChild(div);
+    const label = div.querySelector('label');
+    store.forEach((list) => {
+      if (currentUserEmail === list.currentUser && list.newListName === goal) {
+        list.newListItems.push(label.innerText);
+      }
+    });
+    localStorage.setItem("lists", JSON.stringify(store));
+    toDoListForm.reset();
+    id++;
+  });
+
+  // 1- rename the list.
+  const editListName = document.getElementById("editName");
+  const changeNameForm = document.getElementById("changeNameForm");
+  const header = document.getElementById("head");
+  const div = document.getElementById("changeName");
+  let goallist;
+  let replacedName;
+  editListName.addEventListener("click", (e) => {
+    div.style.display = "none";
+    changeNameForm.style.display = "block";
+  });
+
+  changeNameForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    replacedName = document.getElementById("replaceName").value;
+    header.innerText = replacedName;
+    changeNameForm.style.display = "none";
+    div.style.display = "block";
+    storedLists.forEach((list) => {
+      if (list.currentUser === currentUserEmail && list.newListName === goal) {
+        goallist = list;
+        goallist.newListName = replacedName;
+        localStorage.removeItem("lists");
+        localStorage.setItem("lists", JSON.stringify(storedLists));
+        document.getElementById("listNameText").innerText = replacedName;
+      }
     });
   });
-  const toDoListForm = toDoListPage.querySelector('#toDoListForm');
-
-  toDoListForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const store = JSON.parse(localStorage.getItem('lists'));
-    localStorage.removeItem("'lists");
-    const itemInput = toDoListForm.querySelector('#newItem').value;
-    const li = document.createElement("li");
-    li.innerText=itemInput;
-    ul.appendChild(li);
-    store.forEach(list=>{
-      if(currentUserEmail === list.currentUser && list.newListName === goal){
-        list.newListItems.push(li.innerText);
-      }
-    })
-    localStorage.setItem('lists', JSON.stringify(store));
-    toDoListForm.reset();
-  })
+  const backBtn = document.getElementById("back");
+  backBtn.addEventListener("click", (e) => {
+    toDoListPage.style.display = "none";
+    dashBoard.style.display = "block";
+    mainButtons.style.display = "block";
+    // goallist.newListName = replacedName;
+    document.getElementById("head").innerText = "";
+  });
 
   //Account Settings page
 
@@ -205,21 +323,26 @@ const accountSettings = document.querySelector(".accountSettings");
 const logOutBtn = document.getElementById("logOutBtn");
 
 // to log out
-  logOutBtn.addEventListener("click", (e) => {
-    const toDoListPage = document.querySelector(".toDoList");
+logOutBtn.addEventListener("click", (e) => {
+  const toDoListPage = document.querySelector(".toDoList");
 
-      dashBoard.style.display = "none";
-      startPage.style.display = "block";
-      mainButtons.style.display = "none";
-      toDoListPage.style.display='none';
+  dashBoard.style.display = "none";
+  startPage.style.display = "block";
+  mainButtons.style.display = "none";
+  toDoListPage.style.display = "none";
+  accountSettings.style.display = 'none';
+
 });
 
 // to change account settings
 const accountSettingsBtn = document.getElementById("accountSettingsBtn");
+const toDoListPage = document.querySelector(".toDoList");
+
 accountSettingsBtn.addEventListener("click", (e) => {
   dashBoard.style.display = "none";
   accountSettings.style.display = "block";
   mainButtons.style.display = "block";
+  toDoListPage.style.display = "none";
 });
 
 const createListBtn = document.getElementById("newList");
@@ -236,6 +359,7 @@ function goToBlankList(e) {
     blankList.style.display = "none";
     startPage.style.display = "block";
     mainButtons.style.display = "none";
+    accountSettings.style.display = 'none';
   });
 
   // to change account settings
@@ -244,6 +368,8 @@ function goToBlankList(e) {
     blankList.style.display = "none";
     accountSettings.style.display = "block";
     mainButtons.style.display = "block";
+    toDoListPage.style.display = 'none';
+
   });
 }
 
@@ -304,13 +430,18 @@ saveBtn.addEventListener("click", (e) => {
   allItems.forEach((item) => item.remove());
   blankList.style.display = "none";
   dashBoard.style.display = "block";
-  const store = JSON.parse(localStorage.getItem('lists'))
-  
-  store.forEach(list=>{
+  const store = JSON.parse(localStorage.getItem("lists"));
+  const dashForm = document.getElementById('dashBoardForm');
+  let i = 20;
+  store.forEach(list => {
     if(list.currentUser === currentUserEmail){
-      const li = document.createElement('li');
-      li.textContent=list.newListName;
-      document.getElementById('toDoList').appendChild(li);
+      const div = document.createElement("div");
+      div.innerHTML = `
+    <input type= 'radio' value= '${list.newListName}' id = ${i}> 
+    <label>${list.newListName}</label>
+    `; 
+    i++;
+    dashForm.appendChild(div);
     }
   })
 });
